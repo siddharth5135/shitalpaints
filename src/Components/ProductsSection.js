4e981bb3457818e67f23256b8c96546c46e78743
Siddharth Patel <siddharththpatel@gmail.com>
2024-08-04 11:56:20 +0530

    Header Align Done

--- src/Components/ProductsSection.js
+++ src/Components/ProductsSection.js
@@ -2,15 +2,14 @@ import React, { useCallback, useState } from 'react';
 import { productSegments as segments } from '../productsSegment';
 import { getData as defaultData } from '../Data/Exterior/GetData';
 import { getData as interiorData } from '../Data/Interior/GetData';
-import '../css/Button.css';
+
 import '../css/productsSection.css';
-// import '../css/segment.css';
+import customButton from '../css/Button.module.css';
 
 
 export default function ProductsSection() {
     const [activeIndex, setActiveIndex] = useState(0);
     const [data, setData] = useState(defaultData);
-    const [interiorData, setInteriorData] = useState(interiorData);
     const changeData = useCallback((importedData) => {
         setData(() => importedData);
     }, [setData]);
@@ -20,49 +19,50 @@ export default function ProductsSection() {
             <div className="btn-group" role="group" aria-label="Basic example" style={{ width: '100%', marginTop: '3%', justifyContent: 'space-evenly' }}>
                 {segments.map((item, index) => (
                     <button
-                        className={`button ${activeIndex === index ? 'active' : ''}`}
-                        key={index}
-                        onClick={async () => {
-                            setActiveIndex(index);
-                            try {
-                                switch (index) {
-                                    case 0:
-                                        changeData(defaultData);
-                                        break;
-                                    case 1:
-                                        changeData(interiorData);
-                                        break;
-                                    case 2:
-                                        changeData([]);
-                                        break;
-                                    default:
-                                        changeData([]);
-                                }
-                                document.getElementById('row').classList.add('fade-in');
-                                setTimeout(() => {
-                                    document.getElementById('row').classList.remove('fade-in');
-                                }, 500);
-                            } catch (e) {
-                                console.log(e);
-                                changeData([]);
-                            }
-                        }}
-                    >
-                        {item}
-                    </button>
+                    className={`${customButton.button} ${activeIndex === index ? customButton.active : ''}`}
+                    key={index}
+                    onClick={async () => {
+                      setActiveIndex(index);
+                      try {
+                        switch (index) {
+                          case 0:
+                            changeData(defaultData);
+                            break;
+                          case 1:
+                            changeData(interiorData);
+                            break;
+                            case 2:
+                            changeData([]);
+                            break;
+                          default:
+                            changeData([]);
+                        }
+                        document.getElementById('row').classList.add('fade-in');
+                        setTimeout(() => {
+                          document.getElementById('row').classList.remove('fade-in');
+                        }, 500);
+                      } catch (e) {
+                        console.log(e);
+                        changeData([]);
+                      }
+                    }}
+                  >
+                    {item}
+                  </button>
+          
                 ))}
             </div>
 
             <div className='row' id='row' style={{ justifyContent: 'space-evenly', marginTop: '5%', marginBottom: '5%' }}>
                 {data.length > 0 ? (
                     data.map((item) => (
-                        <div className='card card-main' style={{ width: '18rem', borderRadius: '2rem', marginTop: '1%', flex: 'none' ,alignSelf : 'start'}} key={item.id}>
+                        <div className='card card-main' style={{ width: '18rem', borderRadius: '2rem', height:'80px', marginTop: '1%', flex: 'none' ,alignSelf : 'start'}} key={item.id}>
                             <div style={{ height: '45%', borderRadius: '2.1rem 2.1rem 0 0' }}>
-                                {/* <div className='image-container'> */}
+                                
                                     <figure className='image-container container'>
                                         <img style={{ height: '100%', padding: '6%', display: 'block' }} src={require(`../Images/${item.segment}/${item.imageURL}`)} className="card-img-top img-fluid img-responsive" alt="..." />
                                     </figure>
-                                {/* </div> */}
+                                
                             </div>
 
                             <div className="card-body" style={{ paddingTop: "0rem", paddingBottom: '1rem', flex: 'none' }}>
